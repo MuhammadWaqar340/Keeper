7d78cb5f3d18ae6d8c8124cbec2fd68773423ce7
Muhammad Waqar Ahmed <muhammadwaqarahmed77@gmail.com>
2022-09-07 18:04:52 +0500

    Button Color Changed on hover

--- src/components/practiceComponents/GetTimeByHooks.jsx
+++ src/components/practiceComponents/GetTimeByHooks.jsx
@@ -2,15 +2,30 @@ import { useState } from "react";
 const GetTimeByHooks = () => {
   const now = new Date().toLocaleTimeString();
   const [time, setTime] = useState(now);
+  const [isMouseHover, setMouseHove] = useState(false);
   const updateTime = () => {
     const newTime = new Date().toLocaleTimeString();
     setTime(newTime);
   };
-  setInterval(updateTime, 1000);
+
+  const handleMouseOver = () => {
+    setMouseHove(true);
+  };
+  const handleMouseOut = () => {
+    setMouseHove(false);
+  };
+  //   setInterval(updateTime, 1000);
   return (
     <div>
       <h1>{time}</h1>
-      <button onClick={updateTime}>Get Current Time</button>
+      <button
+        style={{ backgroundColor: isMouseHover ? "black" : "white" }}
+        onClick={updateTime}
+        onMouseOver={handleMouseOver}
+        onMouseOut={handleMouseOut}
+      >
+        Get Current Time
+      </button>
     </div>
   );
 };
